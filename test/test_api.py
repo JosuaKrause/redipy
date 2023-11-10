e9d268cc2f24f99ac8d58ade9ab4c5b82a2bedc0
Josua Krause <josua.krause@gmail.com>
2023-11-09 21:04:26 -0500

    test script in pipeline with pipeline setup

--- test/test_api.py
+++ test/test_api.py
@@ -57,8 +57,10 @@ def test_api(rt_lua: bool) -> None:
 
     def check(
             name: str,
+            *,
             setup: Callable[[str], JSONType],
             normal: Callable[[str], JSONType],
+            setup_pipe: Callable[[PipelineAPI, str], None],
             pipeline: Callable[[PipelineAPI, str], None],
             lua: Callable[[FnContext, KeyVariable], Expr],
             code: str,
@@ -74,11 +76,12 @@ def test_api(rt_lua: bool) -> None:
         assert teardown(key) == output_teardown
         assert result == output
 
-        assert setup(key) == output_setup
         with redis.pipeline() as pipe:
-            pipeline(pipe, key)
-            result, = pipe.execute()
+            assert setup_pipe(pipe, key) is None
+            assert pipeline(pipe, key) is None
+            setup_result, result = pipe.execute()
         assert teardown(key) == output_teardown
+        assert setup_result == output_setup
         assert result == output
 
         ctx = FnContext()
@@ -114,45 +117,49 @@ def test_api(rt_lua: bool) -> None:
         assert teardown(key) == output_teardown
         assert result == output
 
-        assert setup(key) == output_setup
         with redis.pipeline() as pipe:
+            assert setup_pipe(pipe, key) is None
             assert fun(keys={"key": key}, args={}, client=pipe) is None
-            result, = pipe.execute()
+            setup_result, result = pipe.execute()
         assert teardown(key) == output_teardown
+        assert setup_result == output_setup
         assert result == output
 
     check(
         "exists",
-        lambda key: redis.set(key, "a"),
-        lambda key: redis.exists(key),
-        lambda pipe, key: pipe.exists(key),
-        lambda ctx, key: RedisVar(key).exists(),
-        "redis.call(\"exists\", key_0)",
-        lambda key: [redis.get(key), redis.delete(key)],
-        True,
-        1,
-        ["a", 1])
+        setup=lambda key: redis.set(key, "a"),
+        normal=lambda key: redis.exists(key),
+        setup_pipe=lambda pipe, key: pipe.set(key, "a"),
+        pipeline=lambda pipe, key: pipe.exists(key),
+        lua=lambda ctx, key: RedisVar(key).exists(),
+        code="redis.call(\"exists\", key_0)",
+        teardown=lambda key: [redis.get(key), redis.delete(key)],
+        output_setup=True,
+        output=1,
+        output_teardown=["a", 1])
 
     check(
         "lpop",
-        lambda key: redis.lpush(key, "a"),
-        lambda key: redis.lpop(key),
-        lambda pipe, key: pipe.lpop(key),
-        lambda ctx, key: RedisList(key).lpop(),
-        "(redis.call(\"lpop\", key_0) or nil)",
-        lambda key: redis.exists(key),
-        1,
-        "a",
-        0)
+        setup=lambda key: redis.lpush(key, "a"),
+        normal=lambda key: redis.lpop(key),
+        setup_pipe=lambda pipe, key: pipe.lpush(key, "a"),
+        pipeline=lambda pipe, key: pipe.lpop(key),
+        lua=lambda ctx, key: RedisList(key).lpop(),
+        code="(redis.call(\"lpop\", key_0) or nil)",
+        teardown=lambda key: redis.exists(key),
+        output_setup=1,
+        output="a",
+        output_teardown=0)
 
     check(
         "rpop",
-        lambda key: redis.rpush(key, "a"),
-        lambda key: redis.rpop(key),
-        lambda pipe, key: pipe.rpop(key),
-        lambda ctx, key: RedisList(key).rpop(),
-        "(redis.call(\"rpop\", key_0) or nil)",
-        lambda key: redis.exists(key),
-        1,
-        "a",
-        0)
+        setup=lambda key: redis.rpush(key, "a"),
+        normal=lambda key: redis.rpop(key),
+        setup_pipe=lambda pipe, key: pipe.rpush(key, "a"),
+        pipeline=lambda pipe, key: pipe.rpop(key),
+        lua=lambda ctx, key: RedisList(key).rpop(),
+        code="(redis.call(\"rpop\", key_0) or nil)",
+        teardown=lambda key: redis.exists(key),
+        output_setup=1,
+        output="a",
+        output_teardown=0)
